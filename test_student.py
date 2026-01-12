59d58f8fb3518a77ca21af6a06b513171a95f2f8
Prajwalais <prajwalbakaningi7@gmail.com>
2026-01-12 15:55:15 +0530

    Update test_student.py

--- test_student.py
+++ test_student.py
@@ -2,11 +2,11 @@ from student import student_details
 
 def test_student_details():
     expected_output = (
-        "id: 183\n"
-        "name: Prajwal\n"
-        "age: 19\n"
-        "course: CS\n"
+        "Student_Id:183\n"
+        "Student_Name:prajwal\n"
+        "age:19\n"
+        "course:CS\n"
     )
 
-    assert student_details(183, "Prajwal", 19 , "CS") == expected_output
+    assert student_details(183,"prajwal",19,"CS") == expected_output
     
