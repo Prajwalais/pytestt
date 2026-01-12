cda3b66da1a48ed8ecbbc6009a30ca5a56425398
Prajwalais <prajwalbakaningi7@gmail.com>
2026-01-12 15:54:00 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,17 +1,17 @@
-def student_details(student_id, name, age, course):
+def student_details(student_id,name,age,course):
     result = (
-        f"Student ID: {student_id}\n"
-        f"Student Name: {name}\n"
-        f"Student Age: {age}\n"
-        f"Student Course: {course}\n"
+        f"Student_ID:{student_id}\n"
+        f"Student_Name:{name}\n"
+        f"Student_Age:{age}\n"
+        f"Student_Course:{course}\n"
     )
     return result
 
 
 if __name__ == "__main__":
-    id = 183
-    name = "Prajwal"
-    age = 19
-    course = "CS"   
+    id=183
+    name="Prajwal"
+    age=19
+    course="CS"   
 
-    print(student_details(id, name, age, course))
+    print(student_details(id,name,age,course))
